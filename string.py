fb6e14359630affa88fb062a816f21d0f7a92ea2
Rishikesh799 <97303907+Rishikesh799@users.noreply.github.com>
2022-01-20 18:03:04 +0530

    Update string.py

--- string.py
+++ string.py
@@ -90,3 +90,66 @@ x="geek"
 y=int(len(x)/2)
 print(x[:y].upper()+x[y:])
 GEek
+-------------------------------------------------------------------
+-----------------
+x="geek"
+d=[]
+for i in range(len(x)):
+    if i==0 or i==len(x)-1:
+        d.append(x[i].upper())
+    else:
+        d.append(x[i])
+print(("".join(d)))
+
+print(x[0].upper()+x[1:len(x)-1]+x[len(x)-1].upper())
+GeeK
+GeeK
+-----------------------------------------------------------------------------
+x="abcdef"
+y="defghia"
+d=[]
+for i in x:
+    if i in y:
+        d.append(i)
+    else:
+        pass
+print("common chr in both str is :",d,",And their count is :",len(d))
+
+common chr in both str is : ['a', 'd', 'e', 'f'] ,And their count is : 4
+
+----------------------------------------------------------------------------
+x="vic"
+from itertools import permutations
+y=permutations(x)
+for i in y:
+        print("".join(i))
+---------------------------------------------------------------------------------------
+x={"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9,"zero":0}
+y="zero nine one two eight"
+b=y.split()
+s=[]
+for i in b:
+    s.append(str(x[i]))
+print("".join(s))
+-------------------------------------------------------------------------------------------------
+### Word location in string
+x="zero nine one two eight"
+y="nine"
+c=x.split()
+if y in c:
+    print(c.index(y)+1)
+--------------------------------------------
+x="GeeksforGeeks"
+d=2
+def rotation(x):
+    y=x[:d]
+    j=x[len(x)-d:]
+    print("for left rotation:",x[d:]+y)
+    print("For right rotation:",j+x[:len(x)-d])
+rotation(x)
+-------------------------------------------------------
+x="geeks_for_geeks"
+print(x.replace("_",""))
+y=x.split("_")
+print("".join(y).upper())
+"""
